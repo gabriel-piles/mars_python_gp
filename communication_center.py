fd7fc334632bdc3cc0dab580e6e23bcec3ac712a
gabo mint <gabo@gabos-iMac.local>
2017-12-22 13:48:50 +0100

    Test almost completed except test_all_modules

--- communication_center.py
+++ communication_center.py
@@ -19,8 +19,8 @@ class CommunicationCenter(object):
         except ValueError:
             print('Mission aborted: grid initialization error')
             print(f'Attempt to initializate with the following parameters')
-            print(f'x_limit = {x_limit}')
-            print(f'y_limit = {y_limit}')
+            print(f'x limit = {x_limit}')
+            print(f'y limit = {y_limit}')
             return False
 
         return True
@@ -28,11 +28,11 @@ class CommunicationCenter(object):
     def _available_position_rover(self, rover):
         correct_position = True
 
-        if rover.x_position > self._x_limit or rover.x_position < 0:
+        if rover.x_position > self._x_limit or rover.x_position < 0\
+        or rover.y_position > self._y_limit or rover.y_position < 0:
             correct_position = False
-
-        if rover.y_position > self._y_limit or rover.y_position < 0:
-            correct_position = False
+            print('Mission aborted: Rover out of grid error')
+            print(f'Rover: {rover.name}')
 
         for each_rover_name, each_rover in self._rovers_list.items():
             if each_rover_name == rover.name:
@@ -41,11 +41,20 @@ class CommunicationCenter(object):
             if each_rover.x_position == rover.x_position:
                 if each_rover.y_position == rover.y_position:
                     correct_position = False
+                    print('Mission aborted: Rover collision error')
+                    print(f'Rovers: {rover.name} and {each_rover.name}')
                     break
+
+        if not correct_position:
+            print(f'Position attempted:')
+            print(f'x position = {rover.x_position}')
+            print(f'y position = {rover.y_position}')
 
         return correct_position
 
     def initialize_rover(self, name, x_position, y_position, orientation):
+        print(f'Initializating rover:')
+        print(f'{name}, {x_position}, {y_position}, {orientation}')
         new_rover = Rover(name)
         correct_init = new_rover.set_state(x_position, y_position, orientation)
 
@@ -90,13 +99,102 @@ class CommunicationCenter(object):
 
         return command_executed
 
+    def _subtract_grid_size_from_command(self, grid_size_string):
+
+        try:
+            limits_list = grid_size_string.split()
+            correct_size = self.set_grid_size(limits_list[0], limits_list[1])
+        except IndexError:
+            correct_size = self.set_grid_size(grid_size_string, '')
+
+        return correct_size
+
+    @staticmethod
+    def _get_rover_name_by_index(index):
+        return f'Rover {index}'
+
+    def _initialize_rovers_by_commands(self, initialization_commands):
+        correct_init = True
+        rover_index = 1
+
+        for each_command in initialization_commands:
+            values = each_command.split()
+            if len(values) < 3:
+                print('Mission aborted: Rover initialization error')
+                print(f'Attempt to initializate with the following command:')
+                print(f'{each_command}')
+                correct_init = False
+                break
+            else:
+                name = self._get_rover_name_by_index(rover_index)
+                if self.initialize_rover(name, values[0], values[1], values[2]):
+                    rover_index += 1
+                else:
+                    correct_init = False
+                    break
+
+        return correct_init
+
+    def _execute_rovers_commands(self, rovers_commands_list):
+        correct_execution = True
+        rover_index = 1
+
+        for each_rover_commands in rovers_commands_list:
+            rover_name = self._get_rover_name_by_index(rover_index)
+            print('Executing rover commands:')
+            print(f'Rover: {rover_name}')
+
+            clean_commands = each_rover_commands.strip()
+            print(f'Commands: {clean_commands}')
+            
+            for each_command in clean_commands:
+                if not self.execute_command(rover_name, each_command):
+                    correct_execution = False
+                    break
+
+            if correct_execution:
+                rover_index += 1
+            else:
+                break
+
+        return correct_execution
+
     def execute_commands_list(self, commands_list):
-        commands_executed = True
+        self._rovers_list = {}
 
-        return commands_executed
+        correct_execution = True
 
-    def get_commands_from_file(self, file_path):
-        pass
+        if len(commands_list) == 0:
+            print('No commands for execute')
+            return False
+
+        print('Configuring grid size..')
+
+        if not self._subtract_grid_size_from_command(commands_list[0]):
+            correct_execution = False
+        else:
+            del commands_list[0]
+            if not self._initialize_rovers_by_commands(commands_list[::2]):
+                correct_execution = False
+            else:
+                if not self._execute_rovers_commands(commands_list[1::2]):
+                    correct_execution = False
+
+        return correct_execution
+
+    def execute_commands_from_file(self, file_path):
+        commands_list = []
+        with open(file_path) as commands_lines:
+            for each_line in commands_lines:
+                commands_list.append(each_line)
+
+        self.execute_commands_list(commands_list)
+
+    # def __repr__(self):
+    #     for each_rover_index in range(len(self._rovers_list)):
+    #         rover_name = self._get_rover_name_by_index(each_rover_index + 1)
+    #         state = self.get_rover_state(rover_name)
+    #         print(f'{state[0]} {state[1]} {state[2]}')
 
 if __name__ == '__main__':
     communications = CommunicationCenter()
--- test_communication_center.py
+++ test_communication_center.py
@@ -29,6 +29,8 @@ def test_initialize_rover_fail():
     assert not communications.initialize_rover('Rover 2', 0, 1, 'N')
     assert not communications.initialize_rover('Rover 3', -1, 0, 'N')
     assert not communications.initialize_rover('Rover 4', 0, -1, 'N')
+    assert not communications.initialize_rover('Rover 4', 'a', 0, 'N')
+    assert not communications.initialize_rover('Rover 4', 0, 'a', 'N')
 
     communications = CommunicationCenter(x_limit = 5, y_limit = 5)
     assert not communications.initialize_rover('Rover 5', 6, 0, 'N')
@@ -59,46 +61,36 @@ def test_execute_command():
     communications.initialize_rover('Rover W', 0, 1, 'W')
     assert not communications.execute_command('Rover W', 'M')
 
-# def test_execute_commands_list():
-#     communications = CommunicationCenter(x_limit = 2, y_limit = 2)
-#     communications.execute_commands_list(['2 2', '0 0 N'])
-#     assert communications.get_rover_state('Rover 1') == (0, 0, 'N')
-#
-#     communications.execute_commands_list(['2 2', '0 0 N', 'M', '0 2 E', 'R'])
-#     assert communications.get_rover_state('Rover 1') == (0, 1, 'N')
-#     assert communications.get_rover_state('Rover 2') == (0, 2, 'S')
+def test_execute_commands_list():
+    communications = CommunicationCenter(x_limit = 2, y_limit = 2)
+    assert communications.execute_commands_list([])
+    assert communications.execute_commands_list(['2 2', '0 0 N'])
+    assert communications.get_rover_state('Rover 1') == (0, 0, 'N')
 
-# def test_execute_commands_list_fail():
-#     communications = CommunicationCenter()
-#
-#     with pytest.raises(RoverCollisionError):
-#         communications.execute_commands_list(['2 2', '0 0 N', 'M', '0 1 N'])
-#
-#     with pytest.raises(RoverCollisionError):
-#         communications.execute_commands_list(['2 2', '0 0 N', '', '1 1 N', 'LMLM'])
-#
-# def write_commands_file(tmpdir, commands_list):
-#     temporal_file = tmpdir.join('commands.txt')
-#     with open(temporal_file.strpath, 'w') as fp:
-#         for each_command in commands_list:
-#             fp.write(f'{each_command}\n')
-#
-#     return temporal_file
-#
-# def test_get_commands_from_file(tmpdir):
-#     commands = ['5 5', '0 0 N', 'LMLMLMLMM']
-#     commands_file = write_commands_file(tmpdir, commands)
-#
-#     communications = CommunicationCenter()
-#     commands_list = communications.get_commands_from_file(commands_file.strpath)
-#     assert commands_list == commands
-#
-# def test_all_modules(tmpdir):
-#     commands = ['5 5', '1 2 N', 'LMLMLMLMM', '3 3 E', 'MMRMMRMRRM']
-#     commands_file = write_commands_file(tmpdir, commands)
-#
-#     communications = CommunicationCenter()
-#     commands_list = communications.get_commands_from_file(commands_file.strpath)
-#     communications.execute_commands_list(commands_list)
-#
-#     assert print(communications) == '1 3 N\n5 1 E'
+    assert communications.execute_commands_list(['2 2', '0 0 N', 'M', '0 2 E', 'R'])
+    assert communications.get_rover_state('Rover 1') == (0, 1, 'N')
+    assert communications.get_rover_state('Rover 2') == (0, 2, 'S')
+
+def test_execute_commands_list_fail():
+    communications = CommunicationCenter()
+
+    assert not communications.execute_commands_list(['2 2', '0 0 N', 'M', '0 1 N'])
+    assert not communications.execute_commands_list(['2 2', '0 0 N', '', '1 1 N', 'LMLM'])
+
+def write_commands_file(tmpdir, commands_list):
+    temporal_file = tmpdir.join('commands.txt')
+    with open(temporal_file.strpath, 'w') as fp:
+        for each_command in commands_list:
+            fp.write(f'{each_command}\n')
+
+    return temporal_file
+
+def test_all_modules(tmpdir):
+    commands = ['5 5', '1 2 N', 'LMLMLMLMM', '3 3 E', 'MMRMMRMRRM']
+    commands_file = write_commands_file(tmpdir, commands)
+
+    communications = CommunicationCenter()
+    commands_list = communications.execute_commands_from_file(commands_file.strpath)
+    assert communications.get_rover_state('Rover 1') == (1, 3, 'N')
+    assert communications.get_rover_state('Rover 2') == (5, 1, 'E')
+    # assert print(communications) == '1 3 N\n5 1 E'
